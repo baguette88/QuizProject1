867c4e3323cd907865b9fb40e31174e7cb7db2bf
jbrown7407 <jbrown7407@gmail.com>
2020-10-13 13:08:03 -0400

    UPdate

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 $(() => {
   let clickCount = 1
+ 
   let seconds
   let data
-  let cl = (value) => console.log(value); 
-  // cl("Jquery Active")
-  let bonus = 0 //extra time
+  let cl = (value) => console.log(value); // cl("Jquery Active")
+  // let bonus = 0 //extra time
   let level = 1
   let questionNumber = 0
   let playerScore = 0
@@ -51,39 +51,23 @@ $($categorydisplay).appendTo('.categories')
  
 //API LINK
 
-  //18 is SPORTS!!!!
    categoryChoice=18 
    categoryChoice=21    //sports?
    categoryChoice=15   // VIDEO GAMES
        baseURL =` https://opentdb.com/api.php?amount=50&category=`+categoryChoice+`&type=multiple ` 
     
 
-//// 1   COOSE CAT
+
  $(".categoryComputers").click(function categoryComputers(){ //BUTTON "START GAME" 
   categoryChoice=15
 })
 
-
-// let $totalScore =$('<h2>').text(totalScore) //TEXT ON SCREEN
-// $('.topArea').prepend($totalScore)
-
-// function updateScore() {
-//      //add 100 points to score
-//    playerScore = playerScore + 100
-//    $($playerScore).html(playerScore)
-//    cl(playerScore)
-
-     
-
-// }
-
-//// 4 
   function endGame() {
     //if timer = 0
     //freeze the score
   }
 
- //// 4 
+
 $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GAME"
     categoryChoice=18
     //To DO modify URL of API to include categoryCHOICE
@@ -92,52 +76,38 @@ $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GA
 
  //// 5 LOAD API FUNCTION AROUND GETAPI 
 
-
+//function categoryChosen(){
   const apiKey = ``  
   let currentQuestion=0
   
   const queryType = `t=`
  
   let obj
-  /////// Charles helped debug
-  let queryURL = baseURL + apiKey + '&' + queryType
+  let queryURL = baseURL + apiKey + '&' + queryType   
 
 
-
-  // ADD FUNCTION SO THIS DOES NOT RUN IMMEDIATELY ON LOAD (UNTIL AFTER CATEGORY SELECTION)
-     //function startGame()  **BRING UP HERE***
+                                   // ADD FUNCTION SO THIS DOES NOT RUN IMMEDIATELY ON LOAD (UNTIL AFTER CATEGORY SELECTION)
+                                    //function startGame()  **BRING UP HERE***
   
         jQuery.ajax({
-            url:baseURL,
+            url:baseURL,   /////// Charles helped debug
             dataType: 'text',
             type: "GET",
-                
-            
+                 
           success: function(data) {
                 let dataObject = {}
                 obj = JSON.parse(data);
-                
                 dataObject = data
+
                 let category= data.category
-              
                 let myquestions = data
                 console.log(obj.results[1])
-                    // console.log(data)
-                  //FOR LOOP HERE INSTEAD?
-                  // let question=myQuestions[i].question
-                  // let correctAnswer=myQuestions[i].correct_answer
-                  // let incorrectAnswer=myQuestions[i].incorrect_answers
-            
-                  // let selector = Math.ceil(Math.random)*10
                 
-
        const $topArea = $('<topArea>');
        
-
           $(".nextQuestion").click(function nextQuestion(){ //NEXT QUIZ QUESTION
                 // if (seconds > 0){
                clickCount++
-
 
                 $('.playerScreen').hide()
                 $('.canvas').css('border',"2px solid white")
@@ -162,32 +132,12 @@ $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GA
                       const $form = $('<form>')
                       $($form).appendTo($ul)
                    
-                    //  if (questionNumber>questionsAnswered.length-1) { // RESET QUESTION ARRAY IF FINISHED
-                    //      questionNumber=1
-                    //  }
+                   
                   var x= Math.floor(Math.random() * 50);
                   console.log(x)
-                  // if (doNotRepeat.contains(x)) {                 PREVENT REPEAT QUESTIONS HERE!!!
-                  //   x= Math.floor(Math.random() * 50);
-                  // }
-
-
-//                   function inArray(x,doNotRepeat)
-// {
-//                         var count=doNotRepeat.length;
-//                          for(var i=0;i<count;i++)
-//                            {
-//                              if(doNotRepeat[i]===x){
-//                                console.log("match block")
-//                               x= Math.floor(Math.random() * 50)
-//                              }
-//                            }
-//                             return false;
-//                           }
-                  doNotRepeat.push(x)
                  //// questionsCorrect.push(x) ONLY IF CORRECT, OTHER WISE JUST PUSH TO DO NOT REPEAT
+                  doNotRepeat.push(x)
                  
-               
                  $("input[name='playerChoice']").click(function(){
                   console.log("Answer selected")
               });
@@ -220,12 +170,10 @@ $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GA
                         
                              });
 
-
                       //  $('<span>').text(obj.results[x].category).addClass('answer').appendTo('body').css('color','white');
                       $('<category>').html(obj.results[x].category + ". Difficulty Level: " +obj.results[x].difficulty).css('font-size', '18px').appendTo($ul);
-          
+
                       points++
-                   
                       $('<lhead>').html("Question #" + questionNumber + ". " + obj.results[x].question).attr('id',questionNumber).appendTo($lhead); //QUESTION
                       // $('<modal-open>').html($playerScore)
                       $('.canvas').fadeIn(500)                //FADE BACK IN
@@ -233,21 +181,17 @@ $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GA
                       $('.canvas').append($lhead);
                       $('.canvas').append($ul);
 
-
-
                       function moreTime() {
                         cl("add time to clock")// seconds = seconds+5 
                       }            
 
                       function chooseAnswer() {cl("chooseAnswer")
                       isCorrect()
-                      isWrong()
-                 
+                      isWrong()                
                }
                
 
   function isCorrect() {cl(" checking isCorrect...")  //placed within NEXT question function due to Scoping issues
-
   if($(event.target).is('.correct'))  {
     cl("verified correct")
     $(event.target).css('color', "green")
@@ -256,7 +200,8 @@ $(".categoryVideoGames").click(function categoryVideoGames(){ //BUTTON "START GA
     moreTime()
     $('#timer').css('color', "gold")
     $($playerScore).css('color', "gold")
-    setTimeout(function(){ nextQuestion(); }, 1000);
+
+    setTimeout(function(){ nextQuestion(); }, 1000); // AUTO MVOE TO NEXT QUESTION
      //Play "correct" sound
     }
    }
@@ -270,18 +215,13 @@ function isWrong() {cl("checking is wrong...")
     $(event.target).css('color', "red")
     $(event.target).css('text-decoration', "line-through")
     
-  
     setTimeout(function(){ nextQuestion(); }, 1000);
     //Play "wrong" sound
-    
+    }
   }
-  }
-
                          });      
                   }
-
                 }) // END OF ASYNC
-  
                           
   $(".startGame").click(function startGame(){ //BUTTON "START GAME" 
       $($mega).show()
@@ -291,21 +231,19 @@ function isWrong() {cl("checking is wrong...")
       $('.playerScreen').hide()
       $('.playerScreen').hide()
       $('.startGame').hide()
+      // setTimeout(function(){ nextQuestion(); }, 1000);             AUTO START GAME                      // NEXT QUESTION NOT YET DEFINED
 
   
       $('#timer').appendTo('rank').css('color','white')
       let timeoutHandle;
       var ticks= 0
+
     //Adapted from https://stackoverflow.com/questions/52547625/1-minutes-30-second-countdown-timer-javascript
     function countdown(minutes, seconds) {
-      var seconds = 60;
+      var seconds = 120;
       var mins = minutes
-       
-     //TIMER DEVICE AND OUTPUTS
+  
     function tick() {
-      
-
-
       var counter = document.getElementById("timer");
       ticks++
       $('#timer').css('color', "white")
@@ -314,23 +252,25 @@ function isWrong() {cl("checking is wrong...")
       seconds--;
       console.log(seconds+ " seconds remain")
      
-          if (ticks>90){     // TURN RED ON 30 SECONDS LEFT
+          if (ticks>90){                                                        // TURN ORANGE ON 30 SECONDS LEFT
            $('#timer').css('color','orange')
           }
-//// SWITCH STATEMENT HERE
-          if (seconds<=20){     // TURN RED ON 30 SECONDS LEFT
+                                                                        //// SWITCH STATEMENT HERE
+          if (seconds<=15 && minutes == 0){                                     // TURN RED ON 15 SECONDS LEFT
             $('#timer').css('color','red')
            }
 
       counter.innerHTML =
-      currentMinutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+      // currentMinutes.toString() + ":" + (seconds < 10 ? "0" : "") + 
+      String(seconds);
       if (seconds > 0) {
         timeoutHandle = setTimeout(tick, 1000); //one second
       } else {
-           
-        if (seconds = -1) {
+        console.log("timer cp?")
+        openModal()
+        if (seconds=-1 && minutes<1) {
           
-          //THIS CODE IS NOTE TRIGGERING BUT EVERYTHING WORKS
+          //THIS CODE IS NOTE TRIGGERING BUT EVERYTHING WORKS 
           console.log("timer ended?")
     
           //END GAME FUNCTION HERE
@@ -351,21 +291,17 @@ function isWrong() {cl("checking is wrong...")
   function updateScore() {
     //add 100 points to score
   playerScore = playerScore + 100
+  if (playerScore >= highScore){
+    highScore=playerScore
+  }
   seconds = seconds + 5
   $($playerScore).html(playerScore)
-  cl(playerScore)
-
-    
-
+  cl("Score: "+playerScore + ", High Score: "+ highScore)
 }
 
-  $(".statSheet").click(function statSheet(){ //BUTTON "START GAME" 
-    $($mega).hide()
-    $($titlescreen).hide()
-    $('.btn4').hide()
-    $('.categories').hide()
-    $('.playerScreen').show()
-  })
+$(".restart").click(function restart(){ //BUTTON "RESTART GAME" 
+location.reload();
+})
 
    const addH2 = () => {
       let $mega = document.getElementsByClassName('mega')
@@ -382,36 +318,23 @@ function isWrong() {cl("checking is wrong...")
     addH2()
 
     // Citation - Modal declaration adapted from technique learned in Modal lab
-  //MODAL DECLARATIONS AND EVENT HANDLERS
   const $openBtn = $('#openModal'); //variables declared
   const $modal = $('#modal');
-  const $closeBtn = $('#close');
+  const $modaltextbox = $('#modal-textbox');
+  const $restart = $('#restart');
   $modal.css('display', 'none');
-  //Event Handlers
   const openModal = () => {     //open Modal
     $modal.css('display', 'block');
     $modal.css('text-align', 'center');
-
-
+    $($modaltextbox).html("Questions Correct: X"+ questionsCorrect + '<br>'+ "Final Score: "+ playerScore + '<br><br>'+ '<button class="restart" id="restart" href="#"><A HREF="javascript:history.go(0)">Main Menu</button>')
   }
   const closeModal = () => {     //close Modal
     $modal.css('display', 'none');
     location.reload();
-
   }
    //close on START PROGRAM - HOW TO PLAY!
   //Event Listeners for Modal
   $openBtn.on('click', openModal); // REVISE TO OPEN ON QUESTION ANSWER
-  $closeBtn.on('click', closeModal);
   
-      $( ".closeQuiz" ).click(function() {
-        $($mega).hide()
-        $('.btn3').show()
-        $('.btn2').hide()
-        $($titlescreen).show()
-        $('.categories').show()
-          // $($bigCanvas.hide())
-       console.log("hide game canvas")
-     });
    });
    
