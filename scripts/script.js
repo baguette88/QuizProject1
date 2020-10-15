5b222ad696a5241890ad0976f2be12e7cb059082
jbrown7407 <jbrown7407@gmail.com>
2020-10-15 11:41:52 -0400

    First question opens on start game
    Had to duplicate entire function outside of scope. Not dry code but it works

--- scripts/script.js
+++ scripts/script.js
@@ -15,13 +15,6 @@ $(() => {
     landsound1 = loadSound ('JBBcode/QuizProject1/sounds/landsound1.wav')
   }
 
-
-
-
-
-
-
- // landsound.play()//
   let clickCount = 1
   let totalCorrect = 0
   let bonus = 0
@@ -33,6 +26,7 @@ $(() => {
   let playerScore = 0
   let $currentScore =0
   let highScore =0
+  // highScore = localStorage.getItem("highscore", score)// "SCORE""?
   let rank = "Noob "
   let questionsCorrect = []
   let doNotRepeat = [51]
@@ -43,6 +37,13 @@ $(() => {
   let $playerScore = $('<playerScore>')
   let $highScore = $('<highScore>')
   let x = -1 //counter for shuffle
+  let score = 0
+  // if (score == 0) {
+  //   localStorage.getItem("highscore", highScore);      
+  //   highScoreDisplay.innerHTML = highScore
+    
+   
+  // }
       $($playerScore).addClass('score')
     
      
@@ -83,9 +84,7 @@ let $categorydisplay = $('<categoryDisplay>')
 $($categorydisplay).text(categoryDisplay)
 $($categorydisplay).appendTo('.categories')
  
-
 // cATEGORY SELECTION
-
   
    categoryChoice=15   // DEFAULT CATEGIRY AUTO-SELECTED. keep or remove?
        baseURL =` https://opentdb.com/api.php?amount=50&category=`+categoryChoice+`&type=multiple ` 
@@ -93,11 +92,10 @@ $($categorydisplay).appendTo('.categories')
 
       //  const $topArea = $('<topArea>');
        
-       $(".nextQuestion").click(function nextQuestion() { //NEXT QUIZ QUESTION
-             // if (seconds > 0){
-            clickCount++
-
-           
+     $(".nextQuestion").click(function nextQuestion() { //NEXT QUIZ QUESTION
+            // if (seconds > 0){
+          clickCount++
+    
             // completesound.play()
              $('.playerScreen').hide()
              $('.canvas').css('border',"2px solid white")
@@ -128,7 +126,6 @@ $($categorydisplay).appendTo('.categories')
            
               //LOOP redraws question number if player has already had the question
   
-
                   //////////////////////////////// FISHER-YATES SHUFFLE where I have added a variable X to iterate through
                   function shuffle(array) {
                    var i = array.length,
@@ -386,7 +383,6 @@ tick();
 countdown(2); //three minutes  
 }
 
-
 let obj
 function generateCall() {
   const apiKey = ``  
@@ -415,18 +411,20 @@ function generateCall() {
                 let myquestions = data
                 console.log(obj.results[1])
           }})
-        }    // END GENERATE CALL        
-
-          
+        }    // END GENERATE CALL                 
                           
   $(".startGame").click(function startGame(){ //BUTTON "START GAME" 
   // $(counter).appendTo('.banner').css('font-size', '24px')
-      $($mega).show()
+          
+  
+  $($mega).show()
       $($titlescreen).hide()
       $('.btn4').hide()
       $('.categories').hide()
       $('.startGame').hide()
       $($banner).show()
+      
+      nextQuestion()
       // setTimeout(function(){ nextQuestion(); }, 1000);             AUTO START GAME                      // NEXT QUESTION NOT YET DEFINED
       startTime()
      
@@ -445,13 +443,13 @@ function generateCall() {
   //MODIFY POINT VALUES
   playerScore = playerScore+100
   
-
-  
   if (playerScore >= highScore){
     highScore=playerScore
+  //   localStorage.setItem("highscore", score);      
+  
+  // localStorage.setItem(highScore, highScore)
   }
-
-  // seconds = seconds + 5
+  
   $($playerScore).html("Score: "+playerScore).css('float','right').css('margin-top', '-75px')
   $($playerScore).show()
   $($highScore).html("High Score: "+highScore)
@@ -465,20 +463,6 @@ $(".restart").click(function restart(){ //BUTTON "RESTART GAME"
 location.reload();
 })
 
-  //  const addH2 = () => {
-      
-       
-          
-  //          $rank =$('<rank>').text("Rank= " + rank) //TEXT ON SCREEN
-  //         $('.topArea').prepend($rank)
-  //         $($rank).css('display', 'inline')
-  //         $($rank).css('float', 'right')
-  //         $($rank).css('font-size', '36px')
-  //         $('body').prepend("Player Score: "+$playerScore)
-  //         $('body').prepend("High Score: "+$highScore)
-          
-  //       }
-    // addH2()
 
     // Citation - Modal declaration adapted from technique learned in Modal lab
   const $openBtn = $('#openModal'); //variables declared
@@ -496,18 +480,192 @@ location.reload();
     $modal.css('display', 'none');
     location.reload();
   }
-   //close on START PROGRAM - HOW TO PLAY!
-  //Event Listeners for Modal
+
   $openBtn.on('click', openModal); // REVISE TO OPEN ON QUESTION ANSWER
   
-   
-   
-
-
   $($playerScore).appendTo('.banner')
   $($highScore).appendTo('.banner').css('font-size', '24px')
  
   $($banner).hide()
+
+
+//TURN BACK! CODE IS NOT DRY! THIS IS A REPEAT FUNCTION
+
+
+
+  // REPEATS IN ORDER TO MAKE ACCESSIBLE FOR IMMEDIATE DEPLOYMENT
+  function nextQuestion() { //NEXT QUIZ QUESTION
+    // if (seconds > 0){
+  clickCount++
+
+    // completesound.play()
+     $('.playerScreen').hide()
+     $('.canvas').css('border',"2px solid white")
+     $('.canvas').hide() 
+      $('.canvas').empty()   //slideDown(450)
+     $('.canvas').css('border',"2px solid black")           //STYLING THE QUESTION CANVAS 
+     $('.canvas').css('background-color',"lightgreen")       // CHANGE COLOR AT NEW LEVEL?
+     $('.canvas').css('color',"black")
+     
+
+   $('audio#audio1')[0].play()
+
+     // cl(questionNumber + "old") 
+     questionNumber++
+    
+    // playerScore++
+    
+     cl("playerScore= "+ playerScore)
+       
+           const $bigCanvas = $('<bigCanvas>');
+           const $canvas = $('<canvas>');
+           const $lhead = $('<lhead>').attr('id',questionNumber);
+           const $ul = $('<ul>').attr('id',questionNumber);
+           const $li = $('<li>')
+           const $form = $('<form>')
+           $($form).appendTo($ul)
+        
+   
+      //LOOP redraws question number if player has already had the question
+
+          //////////////////////////////// FISHER-YATES SHUFFLE where I have added a variable X to iterate through
+          function shuffle(array) {
+           var i = array.length,
+               j = 0,
+               temp;
+       
+           while (i--) {
+       
+               j = Math.floor(Math.random() * (i+1));
+       
+               // swap randomly chosen element with current element
+               temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;
+       
+           }
+       
+           return array;
+        
+       }
+       
+       var ranNums = shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]);
+       x++    // 
+       console.log(ranNums)
+       shuffledChoice= ranNums[x];
+       console.log(ranNums)
+          // console.log(doNotRepeat)
+          console.log("shuffled CHoice"+ shuffledChoice)
+       if (ranNums.length > 48) {  /// REFRESH QUESTIONS IF EXHAUSTED
+        //  console.log("generate call if maxed")
+      }
+           ////////////////////////////////////
+
+     
+       console.log(x)
+    
+
+           $('<li>').html(obj.results[x].correct_answer).addClass('correct').appendTo($ul);
+
+           $('<li>').html(obj.results[x].incorrect_answers[0]).addClass('wrongAnswer').appendTo($ul);
+         
+           $('<li>').html(obj.results[x].incorrect_answers[1]).addClass('wrongAnswer').appendTo($ul);
+       
+           $('<li>').html(obj.results[x].incorrect_answers[2]).addClass('wrongAnswer').appendTo($ul);
+          
+            doNotRepeat.push(x)
+
+
+           $.fn.shuffleChildren = function() {       //SHUFFLE LI CHILDREN https://css-tricks.com/snippets/jquery/shuffle-children/
+             $.each(this.get(), function(index, list) {
+                 var $list = $(list);     //list saved as JQUERY object
+                 var $find = $list.children();  //select children of list
+         
+                 $find.sort(function() {
+                     return 0.5 - Math.random();   //randomize sort
+                 });
+         
+                 $list.empty();                       //empty list
+                 $find.appendTo($list);                //re-append
+             });
+         };
+         $($ul).shuffleChildren($li);                // shuffle just li in ul
+           
+
+
+               $($ul).click(function select()            // SPAM BLOCKER! 2 count required to select an answer, each new question ups the counter
+                  {  
+                 //  if (event.target.hasClass('correct'))
+
+                 console.log("clickCount= "+clickCount)
+                 if (clickCount >1) {
+                 isCorrect()
+                 isWrong()
+                 clickCount--
+                 } else {
+                   "click count depleted"
+                 }
+             
+                  });
+
+           //  $('<span>').text(obj.results[x].category).addClass('answer').appendTo('body').css('color','white');
+           $('<category>').html(obj.results[x].category + ". Difficulty Level: " +obj.results[x].difficulty+'<br>').appendTo($lhead);
+
+           points++
+           $('<lhead>').html("Question #" + questionNumber + ". " + obj.results[x].question).attr('id',questionNumber).appendTo($lhead); //QUESTION
+           // $('<modal-open>').html($playerScore)
+           $('.canvas').fadeIn(500)                //FADE BACK IN
+           $('body').append($canvas);
+           $('.canvas').append($lhead);
+           $('.canvas').append($ul);
+
+           function moreTime(seconds) {
+             seconds=seconds+5// seconds = seconds+5 
+           }            
+
+           function chooseAnswer() {cl("chooseAnswer")
+           isCorrect()
+           isWrong()                
+    }
+    
+function isCorrect() {cl(" checking isCorrect...")  //placed within NEXT question function due to Scoping issues
+if($(event.target).is('.correct'))  {
+cl("verified correct")
+bonus= bonus+6
+$(event.target).css('color', "green")
+totalCorrect++
+updateScore()
+$('audio#audio3')[0].play()
+
+$('#timer').css('color', "gold")
+$('#timer').text("+5")
+$($playerScore).css('color', "gold")
+questionsCorrect.push(x)
+setTimeout(function(){ nextQuestion(); }, 1100); // AUTO MVOE TO NEXT QUESTION
+//Play "correct" sound
+console.log("correctQuestions" + questionsCorrect)
+}
+}
+
+function isWrong() {cl("checking is wrong...")
+if($(event.target).is('.wrongAnswer')) {
+cl("verified wrong")
+cl($(this))
+$($ul).css('background-color', "lightred")
+$($bigCanvas).css('background-color', "lightred")
+$(event.target).css('color', "red")
+$(event.target).css('text-decoration', "line-through")
+$('.correct').css('color', "green")
+$('audio#audio4')[0].play()
+setTimeout(function(){ nextQuestion(); }, 800);
+//Play "wrong" sound
+}
+}
+              }   //NEXT QUESTION END   
+
+
+
+
 }
 
 )
